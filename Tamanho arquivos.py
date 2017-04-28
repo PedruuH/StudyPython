85b90d0e47575e9ba0c7cfe2506e2522d9ae0d15
Pedro Henrique <pedro.inventador@hotmail.com>
2017-04-28 15:29:27 -0300

    Update Tamanho arquivos.py

--- Tamanho arquivos.py
+++ Tamanho arquivos.py
@@ -1,11 +1,11 @@
 import os, shutil
 os.chdir('C:\\Users\\UFU\\Desktop\\Corel\\CorelDRAW Graphics Suite X7 - 64 Bits')
-a=200000000
+a=100000000
 for folderName, subfolders, filenames in os.walk('C:\\Users\\UFU\\Desktop\\Corel\\CorelDRAW Graphics Suite X7 - 64 Bits'):
     for filename in filenames:
        p=os.path.getsize(os.path.join(folderName+"\\", filename))           
        if p>=a:
-            print("Arquivo maior que 200Mb \n "+ folderName+filename)
+            print("Arquivo maior que 100Mb \n "+ folderName+filename)
             
             
 
